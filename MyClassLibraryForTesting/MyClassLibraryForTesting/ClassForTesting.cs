7b8724cf6e968daf851507d4ad33c79d39e202f0
Anton Vlasiuk <alealessso@gmail.com>
2017-06-11 20:15:11 +0200

    Start

---
+++ MyClassLibraryForTesting/MyClassLibraryForTesting/ClassForTesting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibraryForTesting
+{
+    public class ClassForTesting
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ MyClassLibraryForTesting/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClassLibraryForTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ClassForTesting _tmp;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _tmp = new ClassForTesting();
+        }
+
+        [TestMethod]
+        public void Add_20_2_result_22()
+        {
+            //arange
+            int a = 20;
+            int b = 2;
+            int expectedResult = 22;
+
+            //act
+            int actualResult = _tmp.Add(a, b);
+
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
